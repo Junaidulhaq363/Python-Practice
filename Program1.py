afef2ac39d7f0fbd282a0078398500313a4fd25d
Junaidul Haq <haqjunaidul5@gmail.com>
2022-11-16 23:57:04 +0530

    first-commit

---
+++ Program1.py
@@ -0,0 +1,24 @@
+#variables
+x="My name is khan"
+y=24
+# z=x+y
+
+print(type(x))
+print(type(y))
+# print(z)
+
+#assigning variables syntax
+
+x,y,z = "junaid", "bhaskar", "damini"
+print(x)
+print(y)
+print(z)
+
+a=b=c = "junaid",23,True,"hasan",44
+print(a)
+print(b)
+print(c)
+
+fruits=["mango","banana","Apple"]
+x,y,z=fruits
+print(x,y,z)
